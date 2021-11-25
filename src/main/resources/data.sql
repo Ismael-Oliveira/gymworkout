3bf0ae1649391ae9969fc796d52b26d5101c25c1
Ismael-Oliveira <ismaeloliveira@inatel.br>
2021-11-24 21:14:44 -0300

    populando banco postgre

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO REGISTER(id, register) VALUES(1, 999);
-INSERT INTO GATE_ONLINE (id, quantity_client, serial)  VALUES (1, 0, 0);
-
-INSERT INTO USER_TB (login, password, type_user)  VALUES ('admin@gmail.com', 'admin', 0);
-
-INSERT INTO USER_TB (login, password, type_user)  VALUES ('user@gmail', 'user', 2);
-
-
